a5882d52464fb9c7d58ff433611ff3d4ca5772bf
Cristian259630 <89108343+Cristian259630@users.noreply.github.com>
2025-05-26 13:30:20 -0600

    Update ProfileSection.jsx

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -92,7 +92,7 @@ const styles = {
     marginBottom: '20px',
   },
   profileImage: {
-    width: '90px',
+    width: '60px',
     height: '90px',
     borderRadius: '50%',
     marginRight: '20px',
